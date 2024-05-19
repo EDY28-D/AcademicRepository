8d24dc9922073b34689479ade1e8acee5086d1cf
Junior Edinson Matias Bardales <junior.matias@unas.edu.pe>
2024-05-19 15:49:16 -0500

    Coomit

--- Proyect/Controllers/CalendarController.cs
+++ Proyect/Controllers/CalendarController.cs
@@ -47,7 +47,7 @@ namespace Proyect.Controllers
             }).ToList();
             return Ok(eventos);
         }
-
+        //si se trabajo eh
         [HttpPost("guardar")]
         public ActionResult Guardar([FromBody] CalendarEvent calendarEvent)
         {
